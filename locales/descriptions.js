5d8d855726c7db606c85562fa22c83e8017f1326
TyrionFront <GingerTormund@protonmail.com>
2019-10-10 21:42:30 +0300

    refactor i18next config

---
+++ locales/descriptions.js
@@ -0,0 +1,11 @@
+export default {
+  descriptions: {
+    errors: {
+      404: 'The page was not found.',
+      429: 'Failed to update posts list. Too many requests.',
+      Wrong: 'No rss-feed is found at this URL',
+      SameFeed: 'Current feed is already added',
+      unspecific: 'Something went wrong.',
+    },
+  },
+};
