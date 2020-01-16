6023fbbd07335945cf20405542a4d4e60fe18538
TyrionFront <GingerTormund@protonmail.com>
2020-01-16 18:51:11 +0200

    add 1 more err description

--- locales/descriptions.js
+++ locales/descriptions.js
@@ -2,6 +2,7 @@ export default {
   descriptions: {
     errors: {
       404: 'The page is not found',
+      403: 'The page is found but the access is forbidden',
       429: 'Failed to update posts list. Too many requests',
       Wrong: 'No rss-feed is found at this URL',
       sameFeed: 'Current feed is already added',
